0b56d88e4ec18ea5ca4682332cfa63d524eddc00
NikitaTrTr <tatnv01@gmail.com>
2022-09-18 14:45:27 +0700

    Restructuring of heapsort. Heapsort without using extra memory

--- Task_1_1_1/src/main/java/ru/nsu/ntatarinov/Heapsort.java
+++ Task_1_1_1/src/main/java/ru/nsu/ntatarinov/Heapsort.java
@@ -4,45 +4,19 @@ package ru.nsu.ntatarinov;
  * Heapsort class for sorting arrays with heap sort algorithm.
  */
 public class Heapsort {
-
-    /**
-     * binary heap.
-     */
-    private int[] heap;
-    /**
-     * size of heap.
-     */
-    private int heapLen;
-
-    /**
-     * method for building heapsort object.
-     *
-     * @param array array you want to sort
-     */
-    public Heapsort(int[] array) {
-        this.heap = new int[array.length];
-        if (array.length == 0) {
-            return;
-        }
-        heap[0] = array[0];
-        this.heapLen = 0;
-        for (int i = 1; i < array.length; i++) {
-            add(array[i], heapLen++);
-        }
-    }
-
     /**
      * Moves up new element to its appropriate index in the heap.
      *
      * @param n index of a moved element
+     * @param array array to sort
      */
-    private void siftUp(int n) {
+    private static void siftUp(int n, int[] array) {
         if (n > 0) {
-            if (heap[n] < heap[(n - 1) / 2]) {
-                int tmp = heap[(n - 1) / 2];
-                heap[(n - 1) / 2] = heap[n];
-                heap[n] = tmp;
-                siftUp((n - 1) / 2);
+            if (array[n] < array[(n - 1) / 2]) {
+                int tmp = array[(n - 1) / 2];
+                array[(n - 1) / 2] = array[n];
+                array[n] = tmp;
+                siftUp((n - 1) / 2, array);
             }
         }
     }
@@ -50,64 +24,58 @@ public class Heapsort {
     /**
      * Moves down element to its appropriate index in the heap.
      */
-    private void siftDown(int n, int k) {
+    private static void siftDown(int n, int k, int[] array) {
         int sonId;
         if (2 * n + 1 <= k) {
             if (2 * n + 1 == k) {
                 sonId = 2 * n + 1;
             } else {
-                if (heap[2 * n + 1] < heap[2 * n + 2]) {
+                if (array[2 * n + 1] < array[2 * n + 2]) {
                     sonId = 2 * n + 1;
                 } else {
                     sonId = 2 * n + 2;
                 }
             }
-            if (heap[n] > heap[sonId]) {
-                int tmp = heap[sonId];
-                heap[sonId] = heap[n];
-                heap[n] = tmp;
-                siftDown(sonId, k);
+            if (array[n] > array[sonId]) {
+                int tmp = array[sonId];
+                array[sonId] = array[n];
+                array[n] = tmp;
+                siftDown(sonId, k, array);
             }
         }
     }
-
-    /**
-     * adds new element to heap.
-     *
-     * @param x element
-     * @param k the first empty place in heap
-     */
-    private void add(int x, int k) {
-        heap[k + 1] = x;
-        siftUp(k + 1);
-    }
-
     /**
      * Gives you a 0-indexed element (aka minimum) from a current heap.
      *
      * @param k current heap size
+     * @param array array to sort
      * @return minimal element
      */
-    private int extractMin(int k) {
-        int min = heap[0];
-        heap[0] = heap[k];
-        siftDown(0, k - 1);
+    private static int extractMin(int k, int[] array) {
+        int min = array[0];
+        array[0] = array[k];
+        siftDown(0, k - 1, array);
         return min;
     }
 
     /**
      * sorts array from which heap was built.
      *
-     * @return sorted array
+     * @param array array to sort
      */
-    public int[] sort() {
-        if (heap.length == 0) {
-            return heap;
+    public static void sort(int[] array) {
+        if (array.length != 0) {
+            for (int i = 1; i < array.length; i++) {
+                siftUp(i, array);
+            }
+            for (int i = array.length - 1; i > 0; i--) {
+                array[i] = extractMin(i, array);
+            }
+            for (int i = 0; i < array.length / 2; i++) {
+                int temp = array[i];
+                array[i] = array[array.length - i - 1];
+                array[array.length - i - 1] = temp;
+            }
         }
-        int[] array = new int[heapLen + 1];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = extractMin(heapLen--);
-        }
-        return array;
     }
 }
