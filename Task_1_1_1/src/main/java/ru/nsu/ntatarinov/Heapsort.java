822c7d0135c79e926ec6fb3aa362b9856cd797e0
NikitaTrTr <tatnv01@gmail.com>
2022-09-18 12:58:43 +0700

    Added junit test, workflow, shell script, javadoc, corrected gitignore

--- Task_1_1_1/src/main/java/ru/nsu/ntatarinov/Heapsort.java
+++ Task_1_1_1/src/main/java/ru/nsu/ntatarinov/Heapsort.java
@@ -1,65 +1,113 @@
 package ru.nsu.ntatarinov;
 
-public class Heapsort{
-    public static void siftUp(int n, int[] heap){
-        if (n>0)
-        {
-            if (heap[n]<heap[(n-1)/2])
-            {
-                int tmp = heap[(n-1)/2];
-                heap[(n-1)/2] = heap[n];
+/**
+ * Heapsort class for sorting arrays with heap sort algorithm.
+ */
+public class Heapsort {
+
+    /**
+     * binary heap.
+     */
+    private int[] heap;
+    /**
+     * size of heap.
+     */
+    private int heapLen;
+
+    /**
+     * method for building heapsort object.
+     *
+     * @param array array you want to sort
+     */
+    public Heapsort(int[] array) {
+        this.heap = new int[array.length];
+        if (array.length == 0) {
+            return;
+        }
+        heap[0] = array[0];
+        this.heapLen = 0;
+        for (int i = 1; i < array.length; i++) {
+            add(array[i], heapLen++);
+        }
+    }
+
+    /**
+     * Moves up new element to its appropriate index in the heap.
+     *
+     * @param n index of a moved element
+     */
+    private void siftUp(int n) {
+        if (n > 0) {
+            if (heap[n] < heap[(n - 1) / 2]) {
+                int tmp = heap[(n - 1) / 2];
+                heap[(n - 1) / 2] = heap[n];
                 heap[n] = tmp;
-                siftUp((n-1)/2, heap);
+                siftUp((n - 1) / 2);
             }
         }
     }
-    public static void siftDown(int n, int k, int[] heap)
-    {
-        int sonID;
-        if (2*n+1<=k)
-        {
-            if (2*n+1==k)
-            {
-                sonID = 2*n+1;
+
+    /**
+     * Moves down element to its appropriate index in the heap.
+     */
+    private void siftDown(int n, int k) {
+        int sonId;
+        if (2 * n + 1 <= k) {
+            if (2 * n + 1 == k) {
+                sonId = 2 * n + 1;
+            } else {
+                if (heap[2 * n + 1] < heap[2 * n + 2]) {
+                    sonId = 2 * n + 1;
+                } else {
+                    sonId = 2 * n + 2;
+                }
             }
-            else
-            {
-                if (heap[2*n+1]<heap[2*n+2])
-                    sonID = 2*n+1;
-                else
-                    sonID = 2*n+2;
-            }
-            if (heap[n]>heap[sonID])
-            {
-                int tmp = heap[sonID];
-                heap[sonID] = heap[n];
+            if (heap[n] > heap[sonId]) {
+                int tmp = heap[sonId];
+                heap[sonId] = heap[n];
                 heap[n] = tmp;
-                siftDown(sonID, k, heap);
+                siftDown(sonId, k);
             }
         }
     }
-    public static void add(int x, int k, int[] heap){
-        heap[k+1] = x;
-        siftUp(k+1,heap);
+
+    /**
+     * adds new element to heap.
+     *
+     * @param x element
+     * @param k the first empty place in heap
+     */
+    private void add(int x, int k) {
+        heap[k + 1] = x;
+        siftUp(k + 1);
     }
-    public static int extractMin(int k, int[] heap){
+
+    /**
+     * Gives you a 0-indexed element (aka minimum) from a current heap.
+     *
+     * @param k current heap size
+     * @return minimal element
+     */
+    private int extractMin(int k) {
         int min = heap[0];
         heap[0] = heap[k];
-        siftDown(0, k-1, heap);
+        siftDown(0, k - 1);
         return min;
     }
-    public static void main(String[] args){
 
-        int k = 0;
-        int N = 10;
-        int[] A = {5, 3, 4, 1, 2, 6, 9, 7, 10,8};
-        int[]heap = new int[10];
-        heap[0] = A[0];
-        for (int i = 1; i<N; i++){
-            add(A[i], k++, heap);
+    /**
+     * sorts array from which heap was built.
+     *
+     * @return sorted array
+     */
+    public int[] sort() {
+        if (heap.length == 0) {
+            return heap;
         }
-        for (int i = 0; i < N; i++) {
-            System.out.print(extractMin(k--, heap)+"\n");
+        int[] array = new int[heapLen + 1];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = extractMin(heapLen--);
         }
+        return array;
     }
 }
---
+++ Task_1_1_1/src/test/java/ru/nsu/ntatarinov/HeapsortTest.java
@@ -0,0 +1,58 @@
+package ru.nsu.ntatarinov;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import java.util.Random;
+
+
+public class HeapsortTest {
+
+    @Test
+    void testEmpty(){
+        int[] array = {};
+        int[] result = {};
+        Heapsort heap = new Heapsort(array);
+        assertArrayEquals(result, heap.sort());
+    }
+
+    @Test
+    void testSinglElement(){
+        int[] array = {5};
+        int[] result = {5};
+        Heapsort heap = new Heapsort(array);
+        assertArrayEquals(result, heap.sort());
+    }
+
+    @Test
+    void testSorted(){
+        int[] array = {1,2,3,4,5};
+        int[] result = {1,2,3,4,5};
+        Heapsort heap = new Heapsort(array);
+        assertArrayEquals(result, heap.sort());
+    }
+
+    @Test
+    void testReversedSorted(){
+        int[] array = {5,4,3,2,1};
+        int[] result = {1,2,3,4,5};
+        Heapsort heap = new Heapsort(array);
+        assertArrayEquals(result, heap.sort());
+    }
+
+    @Test
+    void testBigSize(){
+        Random rd = new Random();
+        int[] array = new int[1000000];
+        int[] result = new int[1000000];
+        for (int i = 0; i<1000000; i++){
+            array[i] = rd.nextInt();
+            result[i] = array[i];
+        }
+        Arrays.sort(result);
+        Heapsort heap = new Heapsort(array);
+        assertArrayEquals(result, heap.sort());
+    }
+
+
+
+}
