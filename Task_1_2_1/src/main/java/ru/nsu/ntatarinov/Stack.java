9a2e634014afcc4599166309af52af283597c355
NikitaTrTr <tatnv01@gmail.com>
2022-09-29 16:03:07 +0700

    Add decreasing stack capacity and test for it Minor fixes

--- Task_1_2_1/src/main/java/ru/nsu/ntatarinov/Stack.java
+++ Task_1_2_1/src/main/java/ru/nsu/ntatarinov/Stack.java
@@ -1,5 +1,7 @@
 package ru.nsu.ntatarinov;
 
+import java.util.EmptyStackException;
+
 /**
  * Java implementation of basic stack.
  *
@@ -8,31 +10,36 @@ package ru.nsu.ntatarinov;
 public class Stack<T> {
 
     private int capacity;
-    private int curSize;
+    private int currentSize;
     private T[] stack;
 
     /**
      * Creates object of Stack class.
      */
+    @SuppressWarnings("unchecked")
     public Stack() {
         this.capacity = 5;
-        this.curSize = 0;
+        this.currentSize = 0;
         this.stack = (T[]) new Object[capacity];
     }
 
     /**
      * Add element to top of Stack.
      *
-     * @param elem element to add
+     * @param element element to add
      */
-    public void push(T elem) {
-        if (curSize == capacity) {
+    @SuppressWarnings("unchecked")
+    public void push(T element) {
+        if (element == null){
+            throw new NullPointerException("Can't push null pointed object");
+        }
+        if (currentSize == capacity) {
             this.capacity *= 2;
             T[] biggerStack = (T[]) new Object[capacity];
-            System.arraycopy(stack, 0, biggerStack, 0, curSize);
+            System.arraycopy(stack, 0, biggerStack, 0, currentSize);
             this.stack = biggerStack;
         }
-        this.stack[curSize++] = elem;
+        this.stack[currentSize++] = element;
     }
 
     /**
@@ -40,8 +47,19 @@ public class Stack<T> {
      *
      * @return element from the top of Stack
      */
+    @SuppressWarnings("unchecked")
     public T pop() {
-        return this.stack[--curSize];
+        if (currentSize==0){
+            throw new EmptyStackException();
+        }
+        if (currentSize<=capacity/2){
+            this.capacity = currentSize;
+            T[] lessStack = (T[]) new Object[capacity];
+            System.arraycopy(stack, 0, lessStack, 0, currentSize);
+            this.stack = lessStack;
+        }
+        this.currentSize--;
+        return this.stack[currentSize];
     }
 
     /**
@@ -50,6 +68,9 @@ public class Stack<T> {
      * @param oldStack stack from which you want to add elements
      */
     public void pushStack(Stack<T> oldStack) {
+        if (oldStack == null){
+            throw new NullPointerException("Can't push null pointed object");
+        }
         for (int i = 0; i < oldStack.count(); i++) {
             push(oldStack.stack[i]);
         }
@@ -61,11 +82,21 @@ public class Stack<T> {
      * @param num number of elements you want to pop
      * @return new Stack with num elements
      */
+    @SuppressWarnings("unchecked")
     public Stack<T> popStack(int num) {
-        this.curSize -= num;
+        if (currentSize<num){
+            throw new EmptyStackException();
+        }
+        this.currentSize -= num;
         Stack<T> newStack = new Stack<>();
         for (int i = 0; i < num; i++) {
-            newStack.push(this.stack[this.curSize + i]);
+            newStack.push(this.stack[this.currentSize + i]);
+        }
+        if (currentSize<=capacity/2){
+            this.capacity = currentSize;
+            T[] lessStack = (T[]) new Object[capacity];
+            System.arraycopy(stack, 0, lessStack, 0, currentSize);
+            this.stack = lessStack;
         }
         return newStack;
     }
@@ -76,6 +107,6 @@ public class Stack<T> {
      * @return number of elements in Stack
      */
     public int count() {
-        return this.curSize;
+        return this.currentSize;
     }
 }
--- Task_1_2_1/src/test/java/ru/nsu/ntatarinov/StackTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/ntatarinov/StackTest.java
@@ -1,7 +1,9 @@
 package ru.nsu.ntatarinov;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.EmptyStackException;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -30,7 +32,7 @@ public class StackTest {
 
     @Test
     void countTest() {
-        Stack<Integer> stack = new Stack<Integer>();
+        Stack<Integer> stack = new Stack<>();
         assertEquals(0, stack.count());
         stack.push(2);
         assertEquals(1, stack.count());
@@ -42,20 +44,20 @@ public class StackTest {
 
     @Test
     void increaseCapacityTest() {
-        Stack<Integer> stack = new Stack<Integer>();
+        Stack<Integer> stack = new Stack<>();
         Integer[] array = {2, 7, 4, 8, 5, 3, 9};
-        for (int i = 0; i < array.length; i++) {
-            stack.push(array[i]);
+        for (Integer integer : array) {
+            stack.push(integer);
         }
         assertEquals(7, stack.count());
     }
 
     @Test
     void popMethodTest() {
-        Stack<Integer> stack = new Stack<Integer>();
+        Stack<Integer> stack = new Stack<>();
         Integer[] array = {2, 7, 4, 8, 5, 3, 9};
-        for (int i = 0; i < array.length; i++) {
-            stack.push(array[i]);
+        for (Integer integer : array) {
+            stack.push(integer);
         }
         for (int i = 0; i < array.length; i++) {
             assertEquals(array[array.length - i - 1], stack.pop());
@@ -64,12 +66,12 @@ public class StackTest {
 
     @Test
     void pushStackMethodTest() {
-        Stack<Integer> stack = new Stack<Integer>();
-        Stack<Integer> pushedStack = new Stack<Integer>();
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> pushedStack = new Stack<>();
         Integer[] array = {2, 7, 4, 8, 5, 3, 9};
-        for (int i = 0; i < array.length; i++) {
-            stack.push(array[i]);
-            pushedStack.push(array[i]);
+        for (Integer integer : array) {
+            stack.push(integer);
+            pushedStack.push(integer);
         }
         stack.pushStack(pushedStack);
 
@@ -79,11 +81,11 @@ public class StackTest {
 
     @Test
     void popStackMethodTest() {
-        Stack<Integer> stack = new Stack<Integer>();
-        Stack<Integer> popStack = new Stack<Integer>();
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> popStack;
         Integer[] array = {2, 7, 4, 8, 5, 3, 9};
-        for (int i = 0; i < array.length; i++) {
-            stack.push(array[i]);
+        for (Integer integer : array) {
+            stack.push(integer);
         }
         popStack = stack.popStack(4);
 
@@ -91,6 +93,34 @@ public class StackTest {
         assertEquals(9, popStack.pop());
         assertEquals(3, popStack.pop());
     }
+    @Test
+    void pushNullTest() {
+        Stack<Integer> stack = new Stack<>();
+        assertThrows(NullPointerException.class, () -> stack.push(null));
+        assertThrows(NullPointerException.class, () -> stack.pushStack(null));
+    }
+    @Test
+    void popFromEmptyStackTest() {
+        Stack<Integer> stack = new Stack<>();
+        assertThrows(EmptyStackException.class, () -> stack.pop());
+        assertThrows(EmptyStackException.class, () -> stack.popStack(1));
+    }
 
-
+    @Test
+    void decreasingStackAfterPopTest() {
+        Stack<Integer> stack = new Stack<>();
+        Integer[] array = {2, 7, 4, 8, 5, 3, 9};
+        for (Integer integer : array) {
+            stack.push(integer);
+        }
+        for (int i = 0; i < 4; i++) {
+            stack.pop();
+        }
+        assertEquals(3, stack.count());
+        for (int i = 0; i < 4; i++) {
+            stack.push(0);
+        }
+        stack.popStack(4);
+        assertEquals(3, stack.count());
+    }
 }
