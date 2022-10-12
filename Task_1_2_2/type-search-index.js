dd0fbc4f8c230c2c0b292e9fa214727d2f160c7f
NikitaTrTr <NikitaTrTr@users.noreply.github.com>
2022-10-12 12:38:17 +0000

    Deploying to gh-pages from @ NikitaTrTr/OOP@a7a398d8fc3200d5db92b4b9e5523eafb6c58a6d 🚀

--- Task_1_2_2/type-search-index.js
+++ Task_1_2_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.ntatarinov","l":"Tree"},{"p":"ru.nsu.ntatarinov","l":"treeIterator"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.ntatarinov","l":"Tree"},{"p":"ru.nsu.ntatarinov","l":"TreeIterator"}]
