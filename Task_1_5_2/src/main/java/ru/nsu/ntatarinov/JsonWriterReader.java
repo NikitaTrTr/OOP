46a9e125f798b2215ac679badde183ff813ac35c
NikitaTrTr <tatnv01@gmail.com>
2022-12-22 19:21:24 +0700

    Add cmd input parsing. Implement show function

---
+++ Task_1_5_2/src/main/java/ru/nsu/ntatarinov/JsonWriterReader.java
@@ -0,0 +1,38 @@
+package ru.nsu.ntatarinov;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JsonWriterReader {
+
+    public static void writeToJson(Notebook notebook, String path) throws IOException {
+        Gson gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .create();
+        String json = gson.toJson(notebook);
+        FileWriter writer = new FileWriter(path);
+        writer.write(json);
+        writer.flush();
+    }
+
+    public static Notebook readFromJson(String path) throws IOException {
+        BufferedReader input = new BufferedReader(new FileReader(path));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String buff = input.readLine();
+            if (buff == null) {
+                break;
+            }
+            sb.append(buff).append("\n");
+        }
+        String json = sb.toString();
+        input.close();
+
+        return new Gson().fromJson(json, Notebook.class);
+    }
+}
--- Task_1_5_2/src/main/java/ru/nsu/ntatarinov/Notebook.java
+++ Task_1_5_2/src/main/java/ru/nsu/ntatarinov/Notebook.java
@@ -1,22 +1,45 @@
 package ru.nsu.ntatarinov;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Notebook {
+
     public List<Record> records;
-    public Notebook(){
+
+    public Notebook() {
         this.records = new ArrayList<>();
     }
-    public void addRecord(String title, String text){
+
+    public void addRecord(String title, String text) {
         records.add(new Record(title, text));
     }
-    public void removeRecord(String title){
+
+    public void removeRecord(String title) {
         records.remove(new Record(title, ""));
     }
-    public void showRecords(){
-        for (Record record: records){
+
+    public void showRecords() {
+        for (Record record : records) {
             System.out.println(record);
         }
+    }
+
+    public void showRecords(Date date1, Date date2, String[] keywords) {
+        records.stream()
+            .filter((Record r) -> r.timestamp.after(date1))
+            .filter((Record r) -> r.timestamp.before(date2))
+            .filter((Record r) -> containsKeywords(keywords, r.title))
+            .forEach(System.out::println);
+    }
+
+    private boolean containsKeywords(String[] keywords, String title) {
+        for (String keyword : keywords) {
+            if (title.toUpperCase().contains(keyword.toUpperCase())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
