91b8c8c30118dc064ab89a9aa2fecea0b2a73e75
NikitaTrTr <NikitaTrTr@users.noreply.github.com>
2022-12-25 04:42:08 +0000

    Deploying to gh-pages from @ NikitaTrTr/OOP@07a284ddf2904644cf6d0bb8ef23acc5cf37e415 🚀

---
+++ Task_1_5_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.ntatarinov","l":"InputParser"},{"p":"ru.nsu.ntatarinov","l":"JsonWriterReader"},{"p":"ru.nsu.ntatarinov","l":"Main"},{"p":"ru.nsu.ntatarinov","l":"Notebook"},{"p":"ru.nsu.ntatarinov","l":"Record"}]
