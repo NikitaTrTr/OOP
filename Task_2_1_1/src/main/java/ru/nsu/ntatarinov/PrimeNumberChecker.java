399ed9dd9373066cce9505834362f54f3680d818
NikitaTrTr <tatnv01@gmail.com>
2023-02-28 21:55:52 +0700

    Fix bug in prime checking

--- Task_2_1_1/src/main/java/ru/nsu/ntatarinov/PrimeNumberChecker.java
+++ Task_2_1_1/src/main/java/ru/nsu/ntatarinov/PrimeNumberChecker.java
@@ -6,7 +6,7 @@ public abstract class PrimeNumberChecker {
     public List<Integer> list;
     public abstract boolean check() throws InterruptedException;
     public static boolean isPrime(Integer number){
-        for (int i = 2; i<=Math.sqrt(number)+1; i++){
+        for (int i = 2; i<=Math.sqrt(number); i++){
             if (number % i == 0 )
             {return false;}
         }
