6cdd26f206f2a1a4d01f14afc30def703486ea6b
NikitaTrTr <tatnv01@gmail.com>
2023-02-28 23:56:21 +0700

    Add benchmarks and results

---
+++ Task_2_1_1/src/jmh/java/ru/nsu/ntatarinov/BenchmarkForParallelPrimeChecker.java
@@ -0,0 +1,31 @@
+package ru.nsu.ntatarinov;
+
+import java.util.List;
+
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+/**
+ * Benchmark for parallel prime checker.
+ */
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class BenchmarkForParallelPrimeChecker {
+
+    List<Integer> data = List.of(999999017, 999999029, 999999043,
+        999999059, 999999067, 999999103, 999999107, 999999113, 999999131, 999999137, 999999151,
+        999999163, 999999181, 999999191, 999999193, 999999197, 999999223, 999999229, 999999323);
+
+    @Benchmark
+    public void parallelCheckerTest(Blackhole blackhole) {
+        ParallelStreamChecker checker = new ParallelStreamChecker(data);
+        blackhole.consume(checker.check());
+    }
+}
