cf5765faf6ba8b04a56a0fdf99a3c0a9691d06bd
NikitaTrTr <tatnv01@gmail.com>
2023-06-21 00:24:11 +0700

    Fix javadocs

--- Task_2_3_1/src/test/java/ru/nsu/ntatarinov/SnakeTests.java
+++ Task_2_3_1/src/test/java/ru/nsu/ntatarinov/SnakeTests.java
@@ -4,10 +4,15 @@ package ru.nsu.ntatarinov;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Snake tests.
+ */
 public class SnakeTests {
+
     @Test
-    void wallsPositionException(){
-        int[][] walls = { {8, 5}, {8, 6}, {8, 7}, {8, 8}, {8, 9} };
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new SnakeGameModel(16, 16, 10, 3, walls));
+    void wallsPositionException() {
+        int[][] walls = {{8, 5}, {8, 6}, {8, 7}, {8, 8}, {8, 9}};
+        Assertions.assertThrows(IllegalArgumentException.class,
+            () -> new SnakeGameModel(16, 16, 10, 3, walls));
     }
 }
